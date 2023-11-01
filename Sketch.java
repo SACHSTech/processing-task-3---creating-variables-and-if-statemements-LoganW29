0c713522c5ee25e151a36e0a1b8931a7510d92d3
LoganW29 <Lw1103724>
2023-11-01 08:46:29 -0400

    variables

--- Sketch.java
+++ Sketch.java
@@ -22,8 +22,8 @@ public class Sketch extends PApplet {
     // Generating random numbers  
     Random randomA = new Random();
     Random randomB = new Random();
-    float faceX = randomA.nextInt(400);
-    float faceY = randomB.nextInt(400); 
+    float fltFaceX = randomA.nextInt(400);
+    float fltFaceY = randomB.nextInt(400); 
 
     // Setting the face size
     float faceSize = (150);
@@ -31,22 +31,22 @@ public class Sketch extends PApplet {
   public void draw() {
    
     // Draw the circle for the head
-    ellipse(faceX, faceY, faceSize, faceSize);
+    ellipse(fltFaceX, fltFaceY, faceSize, faceSize);
     
     // Draw the mouth
     fill(170, 0, 0);
-    arc(faceX, faceY + 20, 80, 50, 0, PI);
-    line(faceX - 40, faceY + 20, faceX + 40, faceY + 20);
+    arc(fltFaceX, fltFaceY + 20, 80, 50, 0, PI);
+    line(fltFaceX - 40, fltFaceY + 20, fltFaceX + 40, fltFaceY + 20);
    
     // Draw the white part of the eyes
     fill(255);      
-    ellipse(faceX - 30, faceY - 20, 35, 30);
-    ellipse(faceX +30, faceY - 20, 35, 30);
+    ellipse(fltFaceX - 30, fltFaceY - 20, 35, 30);
+    ellipse(fltFaceX +30, fltFaceY - 20, 35, 30);
 
     // Draw the pupils
     fill(0);
-    ellipse(faceX - 30, faceY - 20, 15, 12);
-    ellipse(faceX + 30, faceY - 20, 15, 12); 
+    ellipse(fltFaceX - 30, fltFaceY - 20, 15, 12);
+    ellipse(fltFaceX + 30, fltFaceY - 20, 15, 12); 
 
     // Showing the current time
     int intHour = hour();  
@@ -58,21 +58,21 @@ public class Sketch extends PApplet {
     text(intMinute, 10, 52);
     
     // Changing the colour of the face based on location
-    if(faceX < 200 && faceY < 200){
+    if(fltFaceX < 200 && fltFaceY < 200){
       fill(170, 250, 0);
     }
-    else if(faceX > 200 && faceY > 200){
+    else if(fltFaceX > 200 && fltFaceY > 200){
       fill(240, 60, 10);
     }
-    else if(faceX > 200 && faceY < 200){
+    else if(fltFaceX > 200 && fltFaceY < 200){
       fill(0, 0, 170);
     }
     else{
       fill(245, 236, 66);
     }
 
-    if((faceX < 200 && faceY < 200) || (faceX > 200 && faceY > 200)){
-      arc(faceX, faceY + 20, 60, 30, 0, PI);
+    if((fltFaceX < 200 && fltFaceY < 200) || (fltFaceX > 200 && fltFaceY > 200)){
+      arc(fltFaceX, fltFaceY + 20, 60, 30, 0, PI);
     }
 
     }   
